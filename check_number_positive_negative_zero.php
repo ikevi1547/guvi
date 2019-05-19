651a87f9d76c9da078c49b6095712ba26d014a14
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-20 00:15:30 +0530

    Add files via upload

---
+++ check_number_positive_negative_zero.php
@@ -0,0 +1,48 @@
+<!-- Template HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project 1</title>
+</head>
+<body>
+    <center>
+        <form method="POST" action="">
+        <input type="text" placeholder="Enter a Number" name="NUM"/>
+        <input type="submit" name="SignValue" value="Click Here to Find the Number sign"/>
+        </form>
+    </center>
+</body>
+</html>
+
+<!-- Logic PHP -->
+<?php
+error_reporting(0);
+class NumberSign
+    {
+        public $Input;
+        function __construct($get_Num = 0){
+            $this->Input = $get_Num;
+        }
+        function ValidateSign(){
+            if($this->Input>0){
+                echo "Number is a Positive Integer";
+            }
+            if($this->Input<0){
+                echo "Number is a Negative Integer";
+            }
+            if($this->Input==0){
+                echo "Number is a Zero";
+            }
+        }
+    }
+
+if (isset($_POST['SignValue'])){
+    $output = new NumberSign($_POST['NUM']);
+    echo "<center>";
+    echo "<h4>OUTPUT</h4>";
+    $output->ValidateSign();
+    echo "</center>";
+}
