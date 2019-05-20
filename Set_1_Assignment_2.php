db1cdff8ce90df413bd7be3ce21e3ebd0d7d921b
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-20 12:44:59 +0530

    Assignment 2 Even or odd find

---
+++ Set_1_Assignment_2.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$type_in =  ctype_digit($in);
+class EvenOrOdd
+    {
+        public $Input;
+        public $type_var;
+        function __construct($get_Num = 0,$type = 0){
+            $this->Input = $get_Num;
+            $this->type_var = $type;
+        }
+        function Validate(){
+        	if($this->type_var==1){
+                $temp_data = $this->Input;
+                if($temp_data%2==0){
+                    echo "Even";
+                }
+                else{
+                    echo "ODD";
+                }
+        	}
+        	else{
+        		echo "Invalid Input";
+        	}
+        }
+    }
+$output = new EvenOrOdd($in,$type_in);
+$output->Validate();
+?>
