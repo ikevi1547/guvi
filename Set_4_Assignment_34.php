92e25693af2b0842711e48ce986cd25891a63ad3
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 20:42:36 +0530

    count lines

---
+++ Set_4_Assignment_34.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$len = strlen($in);
+$count = 1;
+for($i=0;$i<$len;$i++){
+    if($in[$i]==".")
+    {
+        $count += 1;
+    }
+}
+echo $count;
+?>
