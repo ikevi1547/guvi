af5c0b242955668c2c6235fa5b4c3afc7011c8f3
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 22:00:15 +0530

    fibonacci series

---
+++ Set_4_Assignment_40.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$first = 0;
+$second = 1;
+for ($i = 1; $i <= $in; $i++)
+{
+  if($i <= 1){
+    $output = $i;
+  }
+  else
+  {
+    $output = $first + $second;
+    $first = $second;
+    $second = $output;
+  }
+  echo $output;
+}
+?>
