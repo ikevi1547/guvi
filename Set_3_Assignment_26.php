a6fcebfceb211fb86dabb8a1174a8ad9b3e9331d
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 18:54:26 +0530

    sorting

---
+++ Set_3_Assignment_26.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$i = 0 ;
+while(! feof($stdin))  {
+    $result[$i] = fgets($stdin);
+    $i++;
+  }
+$inp = $result[1];
+$len = strlen($inp);
+$j = 0;
+for($i=0;$i<$len;$i++){
+    if($inp[$i]!=" "){
+        $in[$j] .= $inp[$i];
+    }
+    elseif($inp[$i]==" "){
+        $j++;
+    }
+}
+$len = count($in);
+$n=0;
+for ($i=0;$i<$len;$i++){
+    for($j=$i+1;$j<$len;$j++){
+    if($in[$i]>$in[$j]){
+           $temp_data = $in[$j];
+           $in[$j] = $in[$i];
+           $in[$i] = $temp_data;
+       }
+    }
+}
+for($i=0;$i<$result[0];$i++){
+    echo $in[$i];
+    echo " ";
+}
+?>
