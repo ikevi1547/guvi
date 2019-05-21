00250a1dcc783af7fb820b4dd995360a8f98538e
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 12:26:14 +0530

    updated

--- Set_2_Assignment_13.php
+++ Set_2_Assignment_13.php
@@ -2,15 +2,19 @@
 error_reporting(0);
 $stdin = fopen('php://stdin', 'r');
 $in = fgets($stdin);
-if($in>1){
-if($in%2==0||$in%3==0||$in%4==0||$in%5==0||$in%6==0||$in%7==0||$in%8==0||$in%9==0){
-    echo "no";
-}
-else{
+$flag = false;
+for($i = 2; $i <= $in/2; ++$i)
+        {
+            if($in % $i == 0)
+            {
+                $flag = true;
+                break;
+            }
+        }
+if(!$flag){
     echo "yes";
 }
-}
 else{
-	echo "no";
+    echo "no";
 }
 ?>
