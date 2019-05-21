f6e4728ef8c045fdb1d6a14bcdee3fffeeff1443
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 16:00:48 +0530

    finding the armstrong number

--- Set_2_Assignment_17.php
+++ Set_2_Assignment_17.php
@@ -3,43 +3,22 @@ error_reporting(0);
 $stdin = fopen('php://stdin', 'r');
 $in = fgets($stdin);
 $len = strlen($in);
-$dum_index = 0;
-for($i=0;$i<$len;$i++){
-    if($in[$i]!=" "){
-        $arrone[$dum_index] .= $in[$i];
-    }
-    elseif($in[$i]==" "){
-        $dum_index++;
-    }
-}
-$armstrong_output = 0;
-$start = $arrone[0];
-$end = $arrone[1];
-$check_armstrong_number = $start+1;
-while(1)
-{
-$len = strlen($check_armstrong_number);
 $temp_loop = $len;
 $mul = 1;
-$split_value = str_split($check_armstrong_number);
+$armstrong_output = 0;
 for($i=0;$i<$len;$i++){
     while($temp_loop){
-        $mul *= $split_value[$i];
+        $mul *= $in[$i];
         $temp_loop--;
     }
     $armstrong_output += $mul;
     $temp_loop = $len;
     $mul = 1;
 }
-if($armstrong_output==$check_armstrong_number){
-    $output .= $armstrong_output;
-    $output .= " ";
+if($armstrong_output==$in){
+    echo "yes";
 }
-$armstrong_output = 0;
-$check_armstrong_number++;
-if($check_armstrong_number==$end){
-    break;
+else{
+    echo "no";
 }
-}
-echo $output;
 ?>
