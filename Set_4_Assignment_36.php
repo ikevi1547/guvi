8790fe99dc949909c19564721886c829780ac625
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 21:11:39 +0530

    updated special char

--- Set_4_Assignment_36.php
+++ Set_4_Assignment_36.php
@@ -1,12 +1,13 @@
 <?php
 error_reporting(0);
 $stdin = fopen('php://stdin', 'r');
-$in = fgets($stdin);
+$in = strtolower(fgets($stdin));
 $len = strlen($in);
 $count = 0;
+$alphabetics_numeric = array('a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0');
 for($i=0;$i<$len;$i++){
     if($in[$i]!=" "){
-    if(!ctype_alnum($in[$i]))
+    if(!in_array($in[$i],$alphabetics_numeric))
     {
         $count += 1;
     }
