9c2041ae1b3a676357a17f1ae25536374a7a4970
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 19:39:30 +0530

    Print array value with an index

---
+++ Set_3_Assignment_28.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$i = 0 ;
+while(! feof($stdin))  {
+    $result[$i] = fgets($stdin);
+    $i++;
+  }
+  $j = 0;
+  $len = strlen($result[1]);
+for($i=0;$i<$len;$i++){
+    if($result[1][$i]!=" ")
+    {
+        $arrone[$j] .= $result[1][$i];
+    }
+    elseif($result[1][$i]==" ")
+    {
+        $j++;
+    }
+}   
+for($i=0;$i<$result[0];$i++){
+    echo $i.' '.$arrone[$i];
+    echo '</br>';
+}
+?>
