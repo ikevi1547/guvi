ca16415ca9164e399b10fa768e934b8dddb76018
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-21 11:30:54 +0530

    palindrome or not

---
+++ Set_2_Assignment_12.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$len = strlen($in);
+$count = 0;
+for($i=0,$j=$len-1;$i<($len/2);$i++,$j--){
+if($in[$i]==$in[$j]){
+    $count += 1;
+}
+else{
+    break;
+}
+}
+if($len%2!=0){
+    $check = ($len/2)+0.5;
+}
+else{
+    $check = $len/2;
+}
+if($count==$check){
+    echo "yes";
+}
+else{
+    echo "no";
+}
+?>
