73ab5224398dc56f97e480631a7edf008cb6eb2b
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-22 12:31:33 +0530

    find the number is a sqrt of 2

---
+++ Set_5_Assignment_50.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in= fgets($stdin);
+while($in)
+{
+    $output = ($in/2);
+    $in = $output;
+    if($output>1&&$output<2){
+        echo "no";
+        break;
+    }
+    elseif($output==1){
+        echo "yes";
+        break;
+    }
+}
+?>
