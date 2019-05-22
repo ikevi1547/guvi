c0640971cbe8cbcd6870af5b33bbc5b3e921e706
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-22 18:12:05 +0530

    Check Whether the K element is presented or not

---
+++ Set_6_Assignment_58.php
@@ -0,0 +1,56 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$dum_loop = 0; 
+while(!feof($stdin))
+    {
+        $get_array_in[$dum_loop] = fgets($stdin);
+        $dum_loop++;
+    }
+$dum_loop = 0;
+for($i=0;$i<strlen($get_array_in[0]);$i++)
+    {
+        if($get_array_in[0][$i]!=" ")
+            {
+                $arrone[$dum_loop] .= trim($get_array_in[0][$i]);
+            }
+        elseif($get_array_in[0][$i]==" ")
+            {
+                $dum_loop++;
+            }
+    }
+$N = $arrone[0];
+$K = $arrone[1];
+$dum_loop = 0;
+for($i=0;$i<strlen($get_array_in[1]);$i++)
+    {
+        if($get_array_in[1][$i]!=" ")
+            {
+                $arrtwo[$dum_loop] .= trim($get_array_in[1][$i]);
+            }
+        elseif($get_array_in[1][$i]==" ")
+            {
+                $dum_loop++;
+            }
+    }
+for($i=0;$i<$N;$i++)
+    {
+        if($arrtwo[$i]==$K)
+            {
+                $flag = true;
+                break;   
+            }
+        else
+            {
+                $flag = false;
+            }
+    }
+if($flag)
+    {
+        echo "yes";
+    }
+else
+    {
+        echo "no";
+    }
+?>  
