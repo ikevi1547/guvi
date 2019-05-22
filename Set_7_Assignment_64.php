3010d12240a2e5e2f6d5d4cb2edff4414e55caf7
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-22 19:09:53 +0530

    find even or odd from the two number sum

---
+++ Set_7_Assignment_64.php
@@ -0,0 +1,27 @@
+    
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$dum = 0;
+for($i=0;$i<strlen($in);$i++)
+    {
+        if($in[$i]!=" ")
+            {
+                $arrone[$dum] .= $in[$i];
+            }
+        elseif($in[$i]==" ")
+            {
+                $dum++;
+            }
+    }
+$get_num_to_check = $arrone[0] + $arrone[1];
+if($get_num_to_check%2==0)
+    {
+        echo "even";
+    }
+else
+    {
+        echo "odd";
+    }
+?>
