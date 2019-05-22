2b6a553dc89d13e9f6105f3bb83e9435e902ff36
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-22 11:23:25 +0530

    compare two strings

---
+++ Set_5_Assignment_42.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = fgets($stdin);
+$len = strlen($in);
+$dum_index = 0;
+for($i=0;$i<$len;$i++)
+{
+    if($in[$i]!=" ")
+    {
+        $arrone[$dum_index] .= $in[$i];
+    }
+    elseif($in[$i]==" ")
+    {
+        $dum_index++;
+    }
+}
+$string_one = $arrone[0];
+$string_two = $arrone[1];
+if(strlen($string_one)>=strlen($string_two))
+{
+    echo $string_one;
+}
+else
+{
+    echo $string_two;
+}
+
+?>
