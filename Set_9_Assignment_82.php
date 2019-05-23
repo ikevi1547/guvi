dd352145344b92509513a11ae7e1260ad8aa7c2b
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-23 17:05:14 +0530

    print the output with 5 decimal value

--- Set_9_Assignment_82.php
+++ Set_9_Assignment_82.php
@@ -16,9 +16,8 @@ for($i=0;$i<strlen($in);$i++)
     }
 $prod_of_two_num = (float) ($arrone[0]*$arrone[1]);
 $int_val = (int) $prod_of_two_num;
-$get_length = strlen($int_val)+6;
 $out = (float) $prod_of_two_num-$int_val;
-$final_print = $int_val + 0.000001;
+$final_print = $int_val + 0.00000;
 $out += $final_print;
-echo substr($out,0,$get_length);
+echo round($out,5);
 ?>
