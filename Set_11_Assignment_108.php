91ae4717abaa0313b4e414b3f7c90aad2c9ebd1b
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-24 14:48:13 +0530

    Find the min value

---
+++ Set_11_Assignment_108.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$i = 0 ;
+while(! feof($stdin))  {
+    $result[$i] = fgets($stdin);
+    $i++;
+  }
+  $dum = 0;
+for($i=0;$i<strlen($result[0]);$i++)
+{
+    if($result[0][$i]!=" ")
+        {
+            $arr[$dum] .= $result[0][$i]; 
+        }
+    elseif($result[0][$i]==" ")
+        {
+            $dum++;
+        }
+}
+$K = $arr[1];
+$inp = $result[1];
+$len = strlen($inp);
+$j = 0;
+for($i=0;$i<$len;$i++){
+    if($inp[$i]!=" "){
+        $in[$j] .= $inp[$i];
+    }
+    elseif($inp[$i]==" "){
+        $j++;
+    }
+}
+$len = count($in);
+$n=0;
+for ($i=0;$i<$len;$i++){
+    for($j=$i+1;$j<$len;$j++){
+     if($in[$i]>$in[$j]){
+           $temp_data = $in[$j];
+           $in[$j] = $in[$i];
+           $in[$i] = $temp_data;
+       }
+    }
+}
+
+echo $in[$K-1];
+?>
