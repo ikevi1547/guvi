2600f6d9f5b3136c8373968532d933784fe272e0
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-29 00:54:53 -0700

    Numeric Characters find

--- Set_9_Assignment_90.php
+++ Set_9_Assignment_90.php
@@ -2,10 +2,10 @@
 error_reporting(0);
 $stdin = fopen('php://stdin', 'r');
 $in = fgets($stdin);
-$numeric_values = array(0,1,2,3,4,5,6,7,8,9);
+$alphabetics = array('a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z');
 for($i=0;$i<strlen($in);$i++)
     {
-        if(!ctype_alpha(trim($in[$i])))
+        if(!in_array(strtolower($in[$i]),$alphabetics))
             {
                 echo $in[$i];
             }
