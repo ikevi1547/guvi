8164e8dabfa54385036592de6f2f534f23018af8
ikevi1547 <50806187+ikevi1547@users.noreply.github.com>
2019-05-29 01:59:55 -0700

    Lexicographical Order

---
+++ Set_9_Assignment_89.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(0);
+$stdin = fopen('php://stdin', 'r');
+$in = trim(fgets($stdin));
+for($i=0;$i<strlen($in);$i++){
+    for($j=$i+1;$j<strlen($in);$j++)
+    {
+        if($in[$i]>$in[$j])
+        {
+            $temp = $in[$j];
+            $in[$j] = $in[$i];
+            $in[$i] = $temp;
+        }
+    }
+}
+print_r($in);
